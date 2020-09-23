edc26b349b64cd8d8d87cb6091691769392913b1
superwtt <keepgowtt@126.com>
2020-09-23 14:56:15 +0800

    新增生命周期文章

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  title: " ",
+  base: "/noteJS/",
+  description: "Note JS -- JS学习之路",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href: `/favicon.ico`,
+      },
+    ],
+  ],
+  themeConfig: {
+    logo: "/assets/img/logo.jpg",
+    nav: [{ text: "Github", link: "https://github.com/superwtt" }],
+    sidebar: [
+      {
+        title: "Introduction",
+        collapsable: false,
+        children: [["/introduction/introduction.html", "简介"]],
+      },
+      {
+        title: "React",
+        collapsable: false,
+        children: [
+          {
+            title: "生命周期",
+            path: "/react/生命周期.html",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: {
+    "@vuepress/active-header-links": {
+      sidebarLinkSelector: ".sidebar-link",
+      headerAnchorSelector: ".header-anchor",
+    },
+    "@vuepress/search": {
+      search: true, //默认false
+      searchMaxSuggestions: 10, // 默认是5
+    },
+    "@vuepress/nprogress": false, //默认为true，设置为false可以关闭进度条
+  },
+  dest: "./docs/.vuepress/dist",
+  ga: "",
+  evergreen: true,
+};
